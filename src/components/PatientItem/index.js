4a4f2f93093039ecbfa9ef93957af22bd5a83056
Alex Martinez <a.martinez@kubikdata.com>
2021-05-16 20:37:39 +0200

    styles: refactor list item

--- src/components/PatientItem/index.js
+++ src/components/PatientItem/index.js
@@ -2,46 +2,44 @@ import './style.sass';
 
 const PatientItem = ({item, removePatient}) => {
     return (
-        <div className="item">
-                <ul className="item__list">
-                    <li className="item__value">
-                        <div className="item__value--title">
-                            <b>Id:</b>
-                        </div> 
-                        <div>
-                            {item.id}
-                        </div>
-                    </li>
-                    <li className="item__value">
-                        <div className="item__value--title">
-                            <b>Name:</b>
-                        </div> 
-                        <div>
-                            {item.name}
-                        </div>
-                    </li>
-                    <li className="item__value">
-                        <div className="item__value--title">
-                            <b>Surname:</b>
-                        </div> 
-                        <div>
-                            {item.surname}
-                        </div>
-                    </li>
-                    <li className="item__value">
-                        <div className="item__value--title">
-                            <b>Birthdate:</b>
-                        </div> 
-                        <div className="item__value--date">
-                            {item.birthdate}
-                        </div>
-                    </li>
-                    
-                    <li data-testid="remove-btn" className="item__value item__delete" onClick={() => removePatient(item.id)}>
-                        <i className="far fa-trash-alt item__icon"></i>
-                    </li>
-                </ul>
-        </div>
+            <ul className="list">
+                <li className="list__value">
+                    <div className="list__value--title">
+                        <b>Id:</b>
+                    </div> 
+                    <div>
+                        {item.id}
+                    </div>
+                </li>
+                <li className="list__value">
+                    <div className="list__value--title">
+                        <b>Name:</b>
+                    </div> 
+                    <div>
+                        {item.name}
+                    </div>
+                </li>
+                <li className="list__value">
+                    <div className="list__value--title">
+                        <b>Surname:</b>
+                    </div> 
+                    <div>
+                        {item.surname}
+                    </div>
+                </li>
+                <li className="list__value">
+                    <div className="list__value--title">
+                        <b>Birthdate:</b>
+                    </div> 
+                    <div className="list__value--date">
+                        {item.birthdate}
+                    </div>
+                </li>
+                
+                <li data-testid="remove-btn" className="list__value list__delete" onClick={() => removePatient(item.id)}>
+                    <i className="far fa-trash-alt item__icon"></i>
+                </li>
+            </ul>
     );
 }
 
